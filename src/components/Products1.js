5bf4f7b326902283ce8e1925052fbbad4843e60d
Hemarajsgithub <hemaraj2708@gmail.com>
2024-05-06 14:00:13 +0530

    API request file added

--- src/components/Products1.js
+++ src/components/Products1.js
@@ -1,38 +1,53 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 
 const Products1 = () => {
-  let [products, setProducts] = useState([
-    { id: 1, pName: "Oppo", price: 12000, manufacturedIn: "India" },
-    { id: 2, pName: "Vivo", price: 12000, manufacturedIn: "China" },
-    { id: 3, pName: "Apple", price: 12000, manufacturedIn: "Hngkong" },
-    { id: 4, pName: "Samsung", price: 12000, manufacturedIn: "Pakistan" },
-    { id: 5, pName: "Oneplus", price: 12000, manufacturedIn: "Bangladesh" },
-  ]);
+  // JSON - Javascript Object Notation - {  }
+
+  let [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    let fetchData = async () => {
+      let res = await fetch("http://localhost:4000/products", {
+        method : "GET"
+      });
+      
+      let data = await res.json();
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
+
 
   return (
     <div className="container">
       <div className="d-flex justify-content-between">
-        <Button as={Link} to="/" variant="primary">Back to Home</Button>
-        <h1> Total Products </h1>
+        <Button as={Link} to="/" variant="primary">
+          Back to Home
+        </Button>
+        <h1> Total Products : {products.length} </h1>
       </div>
       <section className="row">
-        {products.map((product, ind) => (
-          <Card style={{ width: "18rem" }} className="m-3" key={ind}>
-            <Card.Img
-              variant="top"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlJ2u482NBtw501OOICaZxGrIcxNtG5SuqWQ&s"
-            />
-            <Card.Body>
-              <Card.Title>{product.pName}</Card.Title>
-              <Card.Text> {product.manufacturedIn}</Card.Text>
-              <Card.Text> {product.price}</Card.Text>
-              <Button variant="primary">View Details</Button>
-            </Card.Body>
-          </Card>
-        ))}
+        { products !== null &&
+          products.map((product) => (
+            <Card style={{ width: "18rem", height:"auto" }} className="m-3 d-grid align-item-between" key={product.id}>
+              <article>
+                <Card.Img variant="top" src={product.images[0]} />
+                <Card.Body>
+                  <Card.Title>{product.title}</Card.Title>
+                  <Card.Text> {product.brand}</Card.Text>
+                  <Card.Text> {product.description}</Card.Text>
+                  <Card.Text> {product.price}</Card.Text>
+                </Card.Body>
+              </article>
+              <hr />
+              <center>
+                <Button variant="primary">Add to Card</Button>
+              </center>
+            </Card>
+          ))}
       </section>
     </div>
   );
